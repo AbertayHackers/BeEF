f88ca7953529df4a5fbce1f3473386e34d677e9a
0xmachos <0xmachos@gmail.com>
2018-02-02 16:39:10 +0000

    Refactor to be more PEP 8 compliant
    No functional changes

--- beef.py
+++ beef.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 # Iain Smart
+"""Do stuff with BeEF"""
 
 # Imports
-import sys, subprocess, argparse, requests
+import sys
+import subprocess
+import argparse
+import requests
 import simplejson as json
 
 # Define output messages
@@ -13,7 +17,8 @@ INFO = "\033[1;36m[INFO]\033[0m"
 
 
 def get_args():
-    # Argparse Setup
+    """Argparse Setup"""
+
     parser = argparse.ArgumentParser(description='Pixel for Pixel websites clones using BeEF')
 
     # Beef password
@@ -21,7 +26,8 @@ def get_args():
     # Site to be cloned
     parser.add_argument('site', help='Site you wish to clone. e.g test.com')
     # Mount point
-    parser.add_argument('-m', metavar='MOUNTPOINT', default='/', help='Mount point of cloned site on your BeEF host')
+    parser.add_argument('-m', metavar='MOUNTPOINT', default='/',
+                        help='Mount point of cloned site on your BeEF host')
     # Host IP address
     parser.add_argument('-i', metavar='IP', default='127.0.0.1', help='IP address of your BeEF host')
     # Host port
@@ -29,7 +35,8 @@ def get_args():
     # Beef username
     parser.add_argument('-u', metavar='USERNAME', default='beef', help='Username for beef')
     # Edit Mode
-    parser.add_argument('-e', metavar=('FIND', 'REPLACE'), nargs=2, help='Enables edit mode. E.g. -e string_to_replace string_replacement')
+    parser.add_argument('-e', metavar=('FIND', 'REPLACE'), nargs=2,
+                        help='Enables edit mode. E.g. -e string_to_replace string_replacement')
 
     arguments = parser.parse_args()
 
@@ -37,20 +44,24 @@ def get_args():
 
 
 def get_api_token():
-    # Get BEEF API Token
+    """Get BEEF API Token"""
+
     try:
-        api_token_request = requests.post("http://{}:{}/api/admin/login".format(beef_host, beef_port), data=json.dumps({'username':args.u, 'password':args.password}))
-    except requests.exceptions.ConnectionError as e:
+        api_token_request = requests.post("http://{host}:{port}/api/admin/login"
+                                          .format(host=BEEF_HOST, port=BEEF_PORT),
+                                          data=json.dumps({'username': ARGS.u, 'password': ARGS.password}))
+
+    except requests.exceptions.ConnectionError as err:
         print("{status} Could not get API token".format(status=FATAL))
         print("{status} BeEF is probably not running".format(status=INFO))
         sys.exit(1)
-    except requests.exceptions.RequestException as e:
-        print(e)
+    except requests.exceptions.RequestException as err:
+        print(err)
         sys.exit(1)
 
     try:
         api_token_json = json.loads(api_token_request.text)
-    except json.decoder.JSONDecodeError as e: 
+    except json.decoder.JSONDecodeError as err:
         print("{status} Did not get valid response from BeEF server".format(status=FATAL))
         print("{status} Check BeEF password is correct".format(status=INFO))
         sys.exit(1)
@@ -61,30 +72,41 @@ def get_api_token():
 
 
 def clone_site(api_token):
-    payload = '{' + '"url":"{}", "mount":"{}"'.format(site_to_clone, mount_point) + '}'
-    r = requests.post("http://{}:{}/api/seng/clone_page?token={}".format(beef_host, beef_port, api_token), data = payload)
-    #r.raise_for_status()
-    if r.ok:
-    # Returns True if status_code is less than 400
-        if args.e and not args.i:
-            subprocess.check_output("sed -i.tmp 's/{}/{}/g' /usr/share/beef-xss/extensions/social_engineering/web_cloner/cloned_pages/{}_mod".format(args.e[0], args.e[1], args.site), shell=True)
+    """ Clone site """
+
+    payload = '{' + '"url":"{target}", "mount":"{path}"'.format(target=SITE_TO_CLONE, path=MOUNT_POINT) + '}'
+    request = requests.post("http://{host}:{port}/api/seng/clone_page?token={token}"
+                            .format(host=BEEF_HOST, port=BEEF_PORT, token=api_token), data=payload)
+
+    if request.ok:
+        # Returns True if status_code is less than 400
         print("{status} {site} cloned Sucessfully!".format(status=PASS, site=ARGS.site))
+        if ARGS.e:
+
             print("{status} Editing {site}".format(status=PASS, site=ARGS.site))
+            subprocess.check_output("sed -i.tmp 's/{}/{}/g'\
+                                    /root/Desktop/beef/extensions/social_engineering/web_cloner/cloned_pages/{}_mod"
+                                    .format(ARGS.e[0], ARGS.e[1], ARGS.site), shell=True)
             # use sed to find and replace a string
             # TODO: Path is hard coded, expects BeEF to be installed in /usr/share/beef-xss
-            payload = '{' + '"url":"{}", "mount":"{}"'.format(site_to_clone, mount_point) + ', "use_existing":"true"}'
-            r = requests.post("http://{}:{}/api/seng/clone_page?token={}".format(beef_host, beef_port, api_token), data=payload)
-            if r.ok:
-            # Returns True if status_code is less than 400
+
+            payload = '{' + '"url":"{}", "mount":"{}"'.format(SITE_TO_CLONE, MOUNT_POINT) + ', "use_existing":"true"}'
+
+            request = requests.post("http://{host}:{port}/api/seng/clone_page?token={token}"
+                                    .format(host=BEEF_HOST, port=BEEF_PORT, token=api_token), data=payload)
+
+            if request.ok:
+                # Returns True if status_code is less than 400
                 print("{status} {site} cloned and edited sucessfully!".format(status=PASS, site=ARGS.site))
                 print("{status} Visit your clone at http://{ip}:{port}{mount_point}"
                       .format(status=INFO, ip=ARGS.i, port=ARGS.p, mount_point=MOUNT_POINT))
+
                 sys.exit(0)
-        elif args.e and args.i:
-            sys.exit(1)
+
         else:
             print("{status} Visit your clone at http://{ip}:{port}{mount_point}"
                   .format(status=INFO, ip=ARGS.i, port=ARGS.p, mount_point=MOUNT_POINT))
+
             sys.exit(0)
     else:
 
@@ -95,14 +117,12 @@ def clone_site(api_token):
 
 
 if __name__ == "__main__":
-    args = get_args()
+    ARGS = get_args()
 
-    beef_host = args.i
-    beef_port = args.p
-    mount_point = args.m
-    site_to_clone = "http://{}".format(args.site)
+    BEEF_HOST = ARGS.i
+    BEEF_PORT = ARGS.p
+    MOUNT_POINT = ARGS.m
+    SITE_TO_CLONE = "http://{site}".format(site=ARGS.site)
 
-    api_token = get_api_token()
-    clone_site(api_token)
-
-
+    TOKEN = get_api_token()
+    clone_site(TOKEN)
